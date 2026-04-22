5ec16c73d1d966c24482d8e84ae7a1e2042591b4
chenxiaobo <chan.alex@foxmail.com>
2026-04-22 18:48:28 +0800

    添加错误信息映射。

--- starter/spring-ai-starter-model-turbo-seedance-video/src/main/java/io/github/loncra/ai/turbo/seedance/video/domian/body/TurboSeedanceVideoQueryResponse.java
+++ starter/spring-ai-starter-model-turbo-seedance-video/src/main/java/io/github/loncra/ai/turbo/seedance/video/domian/body/TurboSeedanceVideoQueryResponse.java
@@ -3,11 +3,16 @@ package io.github.loncra.ai.turbo.seedance.video.domian.body;
 import io.github.loncra.ai.turbo.seedance.video.domian.metadata.TurboSeedanceVideoContentMetadata;
 import io.github.loncra.ai.video.QueryVideoResponse;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class TurboSeedanceVideoQueryResponse extends TurboSeedanceVideoSubmitResponse implements QueryVideoResponse {
 
     private String progress;
 
     private TurboSeedanceVideoContentMetadata metadata;
+
+    private Map<String, Object> error = new LinkedHashMap<>();
 
     public TurboSeedanceVideoQueryResponse() {
     }
@@ -26,5 +31,13 @@ public class TurboSeedanceVideoQueryResponse extends TurboSeedanceVideoSubmitRes
 
     public void setMetadata(TurboSeedanceVideoContentMetadata metadata) {
         this.metadata = metadata;
+    }
+
+    public Map<String, Object> getError() {
+        return error;
+    }
+
+    public void setError(Map<String, Object> error) {
+        this.error = error;
     }
 }
